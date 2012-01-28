901877840c07ee59e3222d73be49faf325b28d57
Derek Dowling <dowling@ualberta.ca>
2012-01-28 16:32:15 -0700

    Created signup.php script

--- src/controllers/signup.php
+++ src/controllers/signup.php
@@ -7,7 +7,20 @@
 
     include('./src/utils/DBManager.php');
 
+    open();
 
+    $firstname = $_POST['firstname'];
+    $email = $_POST['email'];
+    $faculty = $_POST['faculty'];
+    $password = $_POST['password'];
+
+    mysql_connect(localhost,$username,$password);
+    @mysql_select_db($database) or die( "Unable to select database");
+
+    $query = "INSERT INTO contacts VALUES ('','$firstname','$email','$password','$faculty')";
+    mysql_query($query);
+
+    mysql_close();
 
 
 ?>
