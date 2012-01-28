d8328ab6dd180360aad3d7d9e6f72c701a1ba130
Matthew Gallivan <gallivan.matt@gmail.com>
2012-01-28 14:01:40 -0700

    Removed html and body from each

--- src/header.php
+++ src/header.php
@@ -1,15 +1,6 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-<html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <div id="header">
-            <h1>HookedOnCampus</h1>
-            <?php
-				include ("views/login.php");
-            ?>
-        </div>
-    </body>
-</html>
+<div id="header">
+    <h1>HookedOnCampus</h1>
+    <?php
+	include ("views/login.php");
+    ?>
+</div>
--- src/views/login.php
+++ src/views/login.php
@@ -1,16 +1,7 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-"http://www.w3.org/TR/html4/loose.dtd">
-<html>
-    <head>
-        <title></title>
-    </head>
-    <body>
-        <div id="login">
-            <form action="insert.php" method="post">
-                <input type="text" value="@ualberta.ca Email" name="email">
-                <input type="text" value="Password" name="password">
-                <input type="Submit">
-            </form>
-        </div>
-    </body>
-</html>
+<div id="login">
+    <form action="insert.php" method="post">
+        <input type="text" value="@ualberta.ca Email" name="email">
+        <input type="text" value="Password" name="password">
+        <input type="Submit">
+    </form>
+</div>
