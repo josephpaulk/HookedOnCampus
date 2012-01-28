9cd45364799083716d6643e3d73a981f2e161223
Matthew Gallivan <gallivan.matt@gmail.com>
2012-01-28 11:59:16 -0700

    Updated with URL redirect.

--- index.php
+++ index.php
@@ -7,34 +7,41 @@
  * To change this template use File | Settings | File Templates.
  */
 
-    /// This is the router logic we can move it out later
-    $requestURI = explode('/', $_SERVER['REQUEST_URI']);
-    $scriptName = explode('/',$_SERVER['SCRIPT_NAME']);
- 
-    for($i= 0;$i < sizeof($scriptName);$i++)
-    {
-          if ($requestURI[$i] == $scriptName[$i])
-           {
-               unset($requestURI[$i]);
-           }
-     }
-     
-    $command = array_values($requestURI);
+/// This is the router logic we can move it out later
+$requestURI = explode('/', $_SERVER['REQUEST_URI']);
+$scriptName = explode('/', $_SERVER['SCRIPT_NAME']);
+
+for ($i = 0; $i < sizeof($scriptName); $i++) {
+	if ($requestURI[$i] == $scriptName[$i]) {
+		unset($requestURI[$i]);
+	}
+}
+
+$command = array_values($requestURI);
 ?>
 
 <html>
-    <h1>Hooked On Campus</h1>
-    <?php 
-   
-        
-        switch($command[0])
-        {
-            case 'login':
-                print('You want the login page');
-                break;
-             default:
-                print('404 Redirect');
-                 break;
-        }
-    ?>
+    <head>
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="/src/css/reset.css" />
+        <link rel="stylesheet" type="text/css" href="/src/css/main.css" />
+        <!-- /CSS -->
+        <title>HookedOnCampus</title>
+    </head>
+    <body>
+        <?php
+		include ("/src/header.php");
+
+		switch($command[0]) {
+			case 'login' :
+				print('You want the login page');
+				break;
+			default :
+				print('404 Redirect');
+				break;
+		}
+
+		include ("/src/footer.php");
+        ?>
+    </body>
 </html>
