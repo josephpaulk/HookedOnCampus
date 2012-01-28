fb15bf38f88edf18070b593e1a040d1516ab8c5d
Peter Lorimer <peter@uwsclient-142-189.uws.ualberta.ca>
2012-01-28 11:41:24 -0700

    First Commit

--- index.php
+++ index.php
@@ -7,6 +7,34 @@
  * To change this template use File | Settings | File Templates.
  */
 
-    include("/utils/DBManager.php");
-
+    /// This is the router logic we can move it out later
+    $requestURI = explode('/', $_SERVER['REQUEST_URI']);
+    $scriptName = explode('/',$_SERVER['SCRIPT_NAME']);
+ 
+    for($i= 0;$i < sizeof($scriptName);$i++)
+    {
+          if ($requestURI[$i] == $scriptName[$i])
+           {
+               unset($requestURI[$i]);
+           }
+     }
+     
+    $command = array_values($requestURI);
 ?>
+
+<html>
+    <h1>Hooked On Campus</h1>
+    <?php 
+   
+        
+        switch($command[0])
+        {
+            case 'login':
+                print('You want the login page');
+                break;
+             default:
+                print('404 Redirect');
+                 break;
+        }
+    ?>
+</html>
