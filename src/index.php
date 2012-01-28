f53c25b699d2d13c4dc1d7b7f6ed7aee94a767b4
Matthew Gallivan <gallivan.matt@gmail.com>
2012-01-28 11:31:48 -0700

    Basics of the HTML/CSS/PHP front-end.

--- src/index.php
+++ src/index.php
@@ -7,6 +7,22 @@
  * To change this template use File | Settings | File Templates.
  */
 
-    include("/utils/DBManager.php");
-
+    // include("/utils/DBManager.php");
+	
 ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+        "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<!-- CSS -->
+		<link rel="stylesheet" type="text/css" href="css/reset.css" />
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
+		<!-- /CSS -->
+		<title>HookedOnCampus</title>
+	</head>
+	<body>
+		<?php include("header.php"); ?>
+		<?php //include("views/insert.php"); ?>
+		<?php include("footer.php"); ?>
+	</body>
+</html>
