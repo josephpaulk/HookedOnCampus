f7704874f727f994c25c2057dbc86af654a09f08
Matthew Gallivan <gallivan.matt@gmail.com>
2012-01-28 23:31:58 -0700

    css

--- src/header.php
+++ src/header.php
@@ -1,13 +1,14 @@
 <html>
-<head>
-    <link rel='stylesheet' type='text/css' href='http://localhost:8888/HookedOnCampus/src/css/reset.css'>
-    <link rel='stylesheet' type='text/css' href='http://localhost:8888/HookedOnCampus/src/css/main.css'>
-</head>
-<body>
-    
-<div id="header">
-    <h1>HookedOnCampus</h1>
-    <?php
-	include ("views/login.php");
-    ?>
-</div>
+	<head>
+		<link rel='stylesheet' type='text/css' href='http://localhost:8888/HookedOnCampus/src/css/reset.css'>
+		<link rel='stylesheet' type='text/css' href='http://localhost:8888/HookedOnCampus/src/css/main.css'>
+		<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Cabin' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div id="header">
+			<h1>HookedOnCampus</h1>
+			<?php
+			include ("views/login.php");
+			?>
+		</div>
--- src/views/login.php
+++ src/views/login.php
@@ -1,6 +1,6 @@
 <div id="login">
     <form action="./src/controllers/login.php" method="post">
-        <input type="text" value="@ualberta.ca Email" name="email">
+        <input type="text" value="@ualberta.ca" name="email">
         <input type="text" value="Password" name="password">
         <input type="Submit" value="Sign In">
     </form>
