4cbf1f1a218d60e40181105a603451049ec06a58
Derek Dowling <dowling@ualberta.ca>
2012-01-29 00:11:31 -0700

    updated questionaire view and questionaire controller

--- src/controllers/questionaire_controller.php
+++ src/controllers/questionaire_controller.php
@@ -12,6 +12,27 @@
 
    require_once '../utils/db.php';
 
-   $db = new DB();
+    $db = new DB();
+    $id = $_SESSION['id'];
+
+    $drinking = $_POST['drinking'];
+    $smoking = $_POST['smoking'];
+    $eating = $_POST['eating'];
+    $out = $_POST['out'];
+    $gaming = $_POST['gaming'];
+
+    $db->sanitize($drinking);
+    $db->sanitize($smoking);
+    $db->sanitize($eating);
+    $db->sanitize($out);
+    $db->sanitize($gaming);
+
+    $query = "INSERT INTO questions (user_id,drinking, smoking, eating, out, gaming) " .
+             "VALUES('$id','$drinking','$smoking','$eating','$out','$gaming')";
+
+    $db->db_query($query);
+
+    $redir = 'Location:../../profile.php?id='.$id;
+    header($redir);
 
 ?>
