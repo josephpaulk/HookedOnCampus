e7abbf49e90625481f94b42a22dd51ca461ee201
Joel Burford <joelburford@gmail.com>
2012-01-29 15:36:12 -0700

    Message functionality

---
+++ message.php
@@ -0,0 +1,29 @@
+<?php
+
+    require_once './src/utils/auth.php';
+
+    //is_auth();
+	//UNCOMMENT OUT TO BOOT TO INDEX IF NOT AUTH
+	//$id = $_SESSION['id'];
+
+    require_once './src/utils/db.php';
+	require_once './src/views/message_view.php';
+
+    include ('./src/header.php');
+
+    $db = new DB();
+	$pid = 5;
+    switch($action)
+    {
+    	/*case 'results':
+    		viewMessage($pid, $db);
+    		break;*/
+        default:
+            viewMessage($pid, $db);
+            break;
+
+    }
+
+    include('./src/footer.php');
+
+?>
