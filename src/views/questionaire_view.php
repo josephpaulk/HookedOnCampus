a737c5f4d118fe04deba649e92d9ba7a53ace37e
Matthew Gallivan <gallivan.matt@gmail.com>
2012-01-29 09:49:19 -0700

    questionnaire

--- src/views/questionaire_view.php
+++ src/views/questionaire_view.php
@@ -22,23 +22,27 @@
                             );
 
 
-        ?>
 
-            <form action="./src/controllers/questionaire_controller.php?id=$id;action=ques" method="post">
-                <?php foreach($category as $cat) {?>
-                        <label for="<?php echo $cat; ?>"><?php echo $questions[$cat]; ?></label><br/>
-                        <select name="<?php echo $cat;  ?>">
-                            <option value="0"><?php echo $questions[$cat]; ?></option>
-                            <option value="5">Very Often</option>
-                            <option value="4">Often</option>
-                            <option value="3">Sometimes</option>
-                            <option value="2">Rarely</option>
-                            <option value="1">Never</option>
-                        </select>
-              <?php } ?>
-                <input type="Submit" value="Submit">
-            </form>
+?>
 
-        <?php
-    }
+<div id="questionaire">
+	<form action="./src/controllers/questionaire_controller.php?id=$id;action=ques" method="post">
+		<?php foreach($category as $cat) {
+		?>
+		<label for="<?php  echo $cat;?>"><?php  echo $questions[$cat];?></label>
+		<br/>
+		<select name="<?php  echo $cat;?>">
+			<option value="0"><?php  echo $questions[$cat];?></option>
+			<option value="5">Very Often</option>
+			<option value="4">Often</option>
+			<option value="3">Sometimes</option>
+			<option value="2">Rarely</option>
+			<option value="1">Never</option>
+		</select>
+		<?php }?>
+		<input type="Submit" value="Submit">
+	</form>
+</div>
+<?php
+}
 ?>
