430d806f339ca2697597244038a5afce8b72d880
Matthew Gallivan <gallivan.matt@gmail.com>
2012-01-28 22:55:28 -0700

    css

--- src/views/login.php
+++ src/views/login.php
@@ -2,6 +2,6 @@
     <form action="./src/controllers/login.php" method="post">
         <input type="text" value="@ualberta.ca Email" name="email">
         <input type="text" value="Password" name="password">
-        <input type="Submit">
+        <input type="Submit" value="Sign In">
     </form>
 </div>
--- src/views/signup_view.php
+++ src/views/signup_view.php
@@ -6,19 +6,21 @@ function signup_form()
 ?>
 <div id="signup">
         <div id="signup-left">
-            <h1>Get Hooked</h1>
         </div>
         <div id="signup-right">
             <form action="./src/controllers/signup_controller.php?action=add" method="post">
-            <input type="text" value="@ualberta.ca Email" name="email">
-            <input type="text" value="First Name" name="firstname">
+            <label for="email">Email</label><br/>
+            <input type="text" value="@ualberta.ca Email" name="email"><br/><br/>
+            <label for="firstname">First Name</label><br/>
+            <input type="text" value="First Name" name="firstname"><br/><br/>
+            <label for="faculty">Faculty</label><br/>
             <select name="faculty">
-                <?php //foreach(SELECT)?>
             	<option value="1">Science</option>
             	<option value="2">Arts</option>
                 <option value="3">Engineering</option>
-            </select>
-            <input type="text" value="Password" name="password">
+            </select><br/><br/>
+            <label for="password">Password</label><br/>
+            <input type="text" value="Password" name="password"><br/><br/>
             <input type="Submit" value="Sign On">
         </form>
         </div>
