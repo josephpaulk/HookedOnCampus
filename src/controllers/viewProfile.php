18ee42cfc23b3ad4a9322b42c0d791474a53f9da
Derek Dowling <dowling@ualberta.ca>
2012-01-28 17:31:08 -0700

    Finished viewProfile controller.

--- src/controllers/viewProfile.php
+++ src/controllers/viewProfile.php
@@ -7,9 +7,13 @@
 
     include("./utils/DBManager.php");
 
-    $query = 'SELECT firstname, department, gender, haircolor';
-    $cols = "";
+    $query = 'SELECT firstname, department' +
+             'FROM user u, faculty f' +
+             'WHERE u.faculty_id = f.id;';
+    $cols = array('firstname','department');
 
-    select($query, $cols);
+    $results = select($query, $cols);
+
+    include('.src/views/viewProfile.php');
     
  ?>
